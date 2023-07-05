2ffe19dfaa25e36c7d1f9e6f84ee8c47ea2963f7
Антон Рябушев <far2raf@Antons-MacBook-Air.local>
2023-07-05 19:14:29 +0200

    first version

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,49 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [start, setStart] = React.useState('');
+  const [finish, setFinish] = React.useState('');
+  const onClick = () => {
+    window.location.href = `https://www.google.com/search?q=start: ${start}, finish: ${finish}`;
+    
+  };
+  const points: any[] = ['21234'];
+  const googleMapLinkCreator = () => {
+    return 'https://www.google.com/maps';
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Do you want to have a sustainable route?</div>
+      <div>
+        <div>
+          Start geo
+        </div>
+        <input 
+          value={start}
+          onChange={e => setStart(e.target.value)}
+        ></input>
+      </div>
+      <div>
+        <div>
+          Finish geo
+        </div>
+        <input 
+          value={finish}
+          onChange={e => setFinish(e.target.value)}
+        ></input>
+      </div>
+      <button onClick={onClick}>Submit</button>
+      <div>
+        { points ? (
+          <>
+            <a 
+            onClick={() => window.location.href=googleMapLinkCreator()}
+            >
+              Click to check your route in Google Map!
+            </a> 
+          </>
+        ) : (<></>)}
+      </div>
     </div>
   );
 }
